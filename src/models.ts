b67b63ecaebd133d64bed08c9d509746ed44ed2a
NeKz <nekzor@users.noreply.github.com>
2023-12-07 18:31:03 +0100

    Fix model

--- src/models.ts
+++ src/models.ts
@@ -15,7 +15,29 @@ export interface ServerRoom {
     canLoginUser?: boolean;
     canLoginAdmin?: boolean;
     canLoginSuperAdmin?: boolean;
-    maplist?: any;
-    systemInfo?: any;
+    maplist?: {
+      UId: string;
+      Name: string;
+      FileName: string;
+      Environnement: string;
+      Author: string;
+      AuthorNickname: string;
+      GoldTime: number;
+      CopperPrice: number;
+      MapType: string;
+      MapStyle: string;
+    }[];
+    systemInfo?: {
+      PublishedIp: string;
+      Port: number;
+      P2PPort: number;
+      TitleId: string;
+      ServerLogin: string;
+      ServerPlayerId: number;
+      ConnectionDownloadRate: number;
+      ConnectionUploadRate: number;
+      IsServer: boolean;
+      IsDedicated: boolean;
+    };
   };
 }
